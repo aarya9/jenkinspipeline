dfdbaf75e4f6f99b8ba5d247fd535c2b55078945
root <root@jenkins.us-central1-a.c.terraform-336010.internal>
2022-01-18 06:59:41 +0000

    second version

--- main.tf
+++ main.tf
@@ -2,8 +2,24 @@ provider "google"{
 	project="terraform-336010"
 	region="us-central1"
 }
+
+resource "google_compute_network" "test_vpc" {
+  name = "terraform-network-sample"
+}
+
 resource "google_compute_instance" "dev"{
 	name         = "devserver"
   	machine_type = "f1-micro"
   	zone         = "us-central1-a"
+	boot_disk {
+		initialize_params {
+			image = "centos-cloud/centos-7"
+		}
+	}
+	
+	network_interface {
+		network = google_compute_network.test_vpc.name
+			access_config {
+		}
+	}
 }
