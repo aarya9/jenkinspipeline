910c89871b6c14d78f0eee0d872ffdfabdea1683
root <root@jenkins.us-central1-a.c.terraform-336010.internal>
2022-01-18 15:34:53 +0000

    commit-one

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
-provider "google"{
-	credentials="/home/raghavjaggu_gmail_com/gcp-key.json"
-	project="terraform-336010"
-	region="us-central1"
+provider "google" {
+  credentials = "/home/raghavjaggu_gmail_com/gcp-key.json"
+  project     = "terraform-336010"
+  region      = "us-central1"
 }
 
 resource "google_compute_network" "test_vpc" {
-  name = "terraform-network-sample"
+  name = "terraform-network-sample-one"
 }
 
-resource "google_compute_instance" "dev"{
-	name         = "devserver"
-  	machine_type = "f1-micro"
-  	zone         = "us-central1-a"
-	boot_disk {
-		initialize_params {
-			image = "centos-cloud/centos-7"
-		}
-	}
-	
-	network_interface {
-		network = google_compute_network.test_vpc.name
-			access_config {
-		}
-	}
+resource "google_compute_instance" "dev" {
+  name         = "devserver-one"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.test_vpc.name
+    access_config {
+    }
+  }
 }
